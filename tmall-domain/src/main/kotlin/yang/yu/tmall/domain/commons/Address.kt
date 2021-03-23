c3aac320c957b4f2f5d81ea9b19e873e9bd96962
yyang <dayatang@gmail.com>
2021-03-23 21:19:08 +0800

    使用数据类

--- tmall-domain/src/main/kotlin/yang/yu/tmall/domain/commons/Address.kt
+++ tmall-domain/src/main/kotlin/yang/yu/tmall/domain/commons/Address.kt
@@ -8,39 +8,11 @@ import javax.persistence.Embeddable
  * 地址值对象
  */
 @Embeddable
-open class Address(
+data class Address(
     val province: String,
     val city: String,
     val detail: String,
     val receiver: String,
     @Column(name = "receiver_phone")
     val receiverPhone: String){
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
-            return true
-        }
-        if (o !is Address) {
-            return false
-        }
-        return province == o.province &&
-                city == o.city &&
-                detail == o.detail &&
-                receiver == o.receiver &&
-                receiverPhone == o.receiverPhone
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(province, city, detail, receiver, receiverPhone)
-    }
-
-    override fun toString(): String {
-        return "Address{" +
-                "province='" + province + '\'' +
-                ", city='" + city + '\'' +
-                ", detail='" + detail + '\'' +
-                ", receiver='" + receiver + '\'' +
-                ", receiverPhone='" + receiverPhone + '\'' +
-                '}'
-    }
 }
