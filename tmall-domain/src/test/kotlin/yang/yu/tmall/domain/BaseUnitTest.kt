d333991b0c087f81c82f367cb115c78207c7b07b
秋水逍遥 <gdyangyu@gmail.com>
2025-05-10 22:18:28 +0800

    完善

--- tmall-domain/src/test/kotlin/yang/yu/tmall/domain/BaseUnitTest.kt
+++ tmall-domain/src/test/kotlin/yang/yu/tmall/domain/BaseUnitTest.kt
@@ -1,5 +1,8 @@
 package yang.yu.tmall.domain
 
 import org.assertj.core.api.WithAssertions
+import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 
+@Ignore
 abstract class BaseUnitTest : WithAssertions
